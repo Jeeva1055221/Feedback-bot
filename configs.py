536f5e6a2723f6b08a58318f7fe11f74953d16ea
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2024-12-25 08:26:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,9 +14,9 @@ class Config(object):
 
     START = str(os.environ.get("START_TEXT", "Hi🙋‍♂️  <a href=https://t.me/{}>{}</a>\n\n Hello {first}\n\n I am CloudXBotz ADMIN Support📢\n\n✪ How can I Help You? Need Help /help "))
 
-    HELP = str(os.environ.get("HELP_TEXT", "<b>Hᴇʟʟᴏ !{} Tʜɪs  Is Oғғɪᴄɪᴀʟ Bᴏᴛ Fᴏʀ Rᴇǫᴜᴇsᴛɪɴɢ [⚒️Staff⚒️] Fʀᴏᴍ Tʜᴇ ADMIN💫 😎 Oғ @Thirai_HDQ & @CloudxAdmin_Bot\n\n【 𝐌𝐨𝐯𝐢𝐞 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭 👇 】\n ➤ Uncharted\n ➤ Uncharted 2022 ✔️\n\n【 𝐖𝐞𝐛 𝐒𝐞𝐫𝐢𝐞𝐬 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭 👇 】\n ➤ Loki S01\n ➤ Loki S01E04\n\n<blockquote>Group Link 🔎:https://t.me/+U2mWbtvr_PU4OTI9</blockquote>\n\n\n📌Rᴇǫᴜᴇsᴛᴇᴅ Mᴏᴠɪᴇ Wɪʟʟ Bᴇ Uᴘᴅᴀᴛᴇᴅ ᴡWɪᴛʜ Iɴ 𝟷ʜʀ Iғ Aᴠᴀɪʟᴀʙʟᴇ📌.\n\n🎁 <u>ᴘʀᴇᴍɪᴜᴍ ғᴇᴀᴛᴜʀᴇs</u> :\n\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋꜱ\n○ ᴅɪʀᴇᴄᴛ ғɪʟᴇs\n○ ᴀᴅ-ғʀᴇᴇ ᴇxᴘᴇʀɪᴇɴᴄᴇ\n○ ʜɪɢʜ-sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ\n○ ᴍᴜʟᴛɪ-ᴘʟᴀʏᴇʀ sᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋs\n○ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs\n○ ꜰᴜʟʟ ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ\n\nIғ Yᴏᴜ Aɴʏ Dᴏᴜʙᴛ Tᴇxᴛ Iɴ Bᴇʟᴏᴡ✍\n\n\nThank You,\nRegards\n@Thirai_HDQ & @CloudxAdmin_Bot &☣️Mooderator☣️ 👇\nThank You,</b>"))
+    HELP = str(os.environ.get("HELP_TEXT", "<b>Hᴇʟʟᴏ 🙋‍♂️ Tʜɪs  Is Oғғɪᴄɪᴀʟ Bᴏᴛ Fᴏʀ Rᴇǫᴜᴇsᴛɪɴɢ Sᴛᴀғғ Fʀᴏᴍ Tʜᴇ Aᴅᴍɪɴ💫 Oғ @Thirai_HDQ & @CloudxAdmin_Bot\n\n【 𝐌𝐨𝐯𝐢𝐞 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭 👇 】\n ➤ Uncharted\n ➤ Uncharted 2022 ✔️\n\n【 𝐖𝐞𝐛 𝐒𝐞𝐫𝐢𝐞𝐬 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭 👇 】\n ➤ Loki S01\n ➤ Loki S01E04\n\nGroup Link 🔎:https://t.me/+U2mWbtvr_PU4OTI9\n\n\n📌Rᴇǫᴜᴇsᴛᴇᴅ Mᴏᴠɪᴇ Wɪʟʟ Bᴇ Uᴘᴅᴀᴛᴇᴅ ᴡWɪᴛʜ Iɴ 𝟷ʜʀ Iғ Aᴠᴀɪʟᴀʙʟᴇ📌.\n\n🎁 <u>Pʀᴇᴍɪᴜᴍ Fᴇᴀᴛᴜʀᴇs</u> :\n\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋꜱ\n○ ᴅɪʀᴇᴄᴛ ғɪʟᴇs\n○ ᴀᴅ-ғʀᴇᴇ ᴇxᴘᴇʀɪᴇɴᴄᴇ\n○ ʜɪɢʜ-sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ\n○ ᴍᴜʟᴛɪ-ᴘʟᴀʏᴇʀ sᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋs\n○ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs\n○ ꜰᴜʟʟ ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ\n\nIғ Yᴏᴜ Aɴʏ Dᴏᴜʙᴛ Tᴇxᴛ Iɴ Bᴇʟᴏᴡ✍\n\n\nThank You,\nRegards\n@Thirai_HDQ & @CloudXbotz \nThank You,</b>"))
 
-    DONATE = str(os.environ.get("DONATE_TEXT", "pklinkzzadmin@ybl"))
+    DONATE = str(os.environ.get("DONATE_TEXT", "Dᴇᴀʀ Usᴇʀs 🍁"))
 
     DONATE_LINK = str(os.environ.get("DONATE_LINK", "https://telegra.ph/file/8a6d4ed449d76c394d8be.jpg"))
 
