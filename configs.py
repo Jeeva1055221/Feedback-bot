ff9a99aa184516d07d81980b9fcc64ea762cd692
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2024-12-25 09:17:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config(object):
 
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1397269319").split())
 
-    START = str(os.environ.get("START_TEXT", "<b> I am CloudXBotz ADMIN Support📢\n\n✪ How can I Help You? Need Help /help </b> "))
+    START = str(os.environ.get("START_TEXT", "<b> Dᴇᴀʀ Usᴇʀs 🍁 I am CloudXBotz ADMIN Support📢\n\n✪ How can I Help You? Need Help /help </b> "))
 
     HELP = str(os.environ.get("HELP_TEXT", "<b>Hᴇʟʟᴏ 🙋‍♂️ Tʜɪs  Is Oғғɪᴄɪᴀʟ Bᴏᴛ Fᴏʀ Rᴇǫᴜᴇsᴛɪɴɢ Sᴛᴀғғ Fʀᴏᴍ Tʜᴇ Aᴅᴍɪɴ💫 Oғ @Thirai_HDQ & @CloudxAdmin_Bot\n\n【 𝐌𝐨𝐯𝐢𝐞 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭 👇 】\n ➤ Uncharted\n ➤ Uncharted 2022 ✔️\n\n【 𝐖𝐞𝐛 𝐒𝐞𝐫𝐢𝐞𝐬 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭 👇 】\n ➤ Loki S01\n ➤ Loki S01E04\n\nGroup Link 🔎:https://t.me/+U2mWbtvr_PU4OTI9\n\n\n📌Rᴇǫᴜᴇsᴛᴇᴅ Mᴏᴠɪᴇ Wɪʟʟ Bᴇ Uᴘᴅᴀᴛᴇᴅ ᴡWɪᴛʜ Iɴ 𝟷ʜʀ Iғ Aᴠᴀɪʟᴀʙʟᴇ📌.\n\n🎁 <u>Pʀᴇᴍɪᴜᴍ Fᴇᴀᴛᴜʀᴇs</u> :\n\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋꜱ\n○ ᴅɪʀᴇᴄᴛ ғɪʟᴇs\n○ ᴀᴅ-ғʀᴇᴇ ᴇxᴘᴇʀɪᴇɴᴄᴇ\n○ ʜɪɢʜ-sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ\n○ ᴍᴜʟᴛɪ-ᴘʟᴀʏᴇʀ sᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋs\n○ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs\n○ ꜰᴜʟʟ ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ\n\nIғ Yᴏᴜ Aɴʏ Dᴏᴜʙᴛ Tᴇxᴛ Iɴ Bᴇʟᴏᴡ✍\n\n\nThank You,\nRegards\n@Thirai_HDQ & @CloudXbotz \nThank You,</b>"))
 
